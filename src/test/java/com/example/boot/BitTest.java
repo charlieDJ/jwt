3b870099d34471e0c16e81f09b6d909675bdaab5
dj278016981@gmail.com <13558665407dj>
2019-10-25 11:34:46 +0800

    换行符1

--- src/test/java/com/example/boot/BitTest.java
+++ src/test/java/com/example/boot/BitTest.java
@@ -13,12 +13,13 @@ public class BitTest {
         System.out.println(a + "有符号右移：" +Integer.toBinaryString(a >> 2)+",符号存在");
         // 无符号右移
         System.out.println(a + "无符号右移：" +Integer.toBinaryString(a >>> 2)+"，符号消失，被0填充，0被省略");
-
+        System.out.println();
 
         System.out.println("################################java 负数 左移###############################");
         int b = Integer.MIN_VALUE;
         System.out.println(b + "的二进制形式：" + Integer.toBinaryString(b));
         System.out.println(b + "左移：" + Integer.toBinaryString(b << 2));
+        System.out.println("换行符1");
 
         System.out.println("##################################java 正数 左移##############################");
         int c = 5;
