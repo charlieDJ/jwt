5f4db1d356db602c6eb0687ddd484c0844ca1929
dj <dj278016981@gmail.com>
2020-05-11 11:10:02 +0800

    利用二分搜索求解平方根

---
+++ src/test/java/com/example/boot/algorithm/mySqrt/Solution.java
@@ -0,0 +1,33 @@
+package com.example.boot.algorithm.mySqrt;
+
+class Solution {
+
+
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        int l = 1, r = x;
+        int res = 0;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (m == x / m) {
+                // 如果 m 的平方等于 x，直接返回
+                return m;
+            } else if (m > x / m) {
+                // 如果 m 的平方大于 x，说明 x 的平方根在 m 的左侧，更新 r 的值
+                r = m - 1;
+            } else {
+                l = m + 1;
+                res = m;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println("solution.mySqrt() = " + solution.mySqrt(17));
+    }
+
+}
